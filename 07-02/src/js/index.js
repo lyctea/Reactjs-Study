df0573f5c2a3190ff2ea8f95d2c6d70e59fd338f
lyctea <396136337@qq.com>
2017-02-27 12:15:49 +0800

    react组件

---
+++ 07-02/src/js/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import ComponentHeader from './components/header'
+
+class Index extends React.Component{
+  render(){
+    return (
+      <ComponentHeader/>
+    )
+  }
+}
